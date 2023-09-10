750e908f4183ddc688aedd2e4c743e2123aa6685
estevan-r <120763362+estevan-r@users.noreply.github.com>
2023-09-09 19:53:53 -0600

    Define logic for round

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3)+1;
 
     if (randomNum === 1) {
-        console.log("Rock!");
+        return "Rock";
     } else if (randomNum === 2) {
-        console.log("Paper!");
+        return "Paper";
     } else if (randomNum === 3) {
-        console.log("Scissors!");
+        return "Scissors";
     } else {
-        console.log("Not Working.");
+        return "Not Working";
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "Youn Win! Paper beats Rock";
+    } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beat paper";
+    } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        return "You Lose! Scissors beat paper";
+    } else {
+        return "It's a Draw! Play again";
     }
 }
