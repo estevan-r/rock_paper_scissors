cbd4ef84495430e477491e5a09a87c7f31b3e149
estevan-r <120763362+estevan-r@users.noreply.github.com>
2023-09-09 20:10:49 -0600

    Fix to case insensitivity for user

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3)+1;
 
     if (randomNum === 1) {
-        return "Rock";
+        return "rock";
     } else if (randomNum === 2) {
-        return "Paper";
+        return "paper";
     } else if (randomNum === 3) {
-        return "Scissors";
+        return "scissors";
     } else {
         return "Not Working";
     }
@@ -16,17 +16,17 @@ const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 
 function playRound(user, computer) {
-    if (user === "Rock" && computer === "Scissors") {
+    if (user.toLowerCase() === "rock" && computer === "scissors") {
         return "You Win! Rock beats Scissors";
-    } else if (computer === "Rock" && user === "Scissors") {
+    } else if (computer === "rock" && user.toLowerCase() === "scissors") {
         return "You Lose! Rock beats Scissors"
-    } else if (user === "Paper" && computer === "Rock") {
+    } else if (user.toLowerCase() === "paper" && computer === "rock") {
         return "Youn Win! Paper beats Rock";
-    } else if (computer === "Paper" && user === "Rock") {
+    } else if (computer === "paper" && user.toLowerCase() === "rock") {
         return "You Lose! Paper beats Rock";
-    } else if (user === "Scissors" && computer === "Paper") {
+    } else if (user.toLowerCase() === "scissors" && computer === "paper") {
         return "You Win! Scissors beat paper";
-    } else if (computer === "Scissors" && user === "Paper") {
+    } else if (computer === "scissors" && user.toLowerCase() === "paper") {
         return "You Lose! Scissors beat paper";
     } else {
         return "It's a Draw! Play again";
